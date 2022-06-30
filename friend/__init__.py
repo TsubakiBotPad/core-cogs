64202d6df686045a7b259d6361214589876e4d55
AA <chasehult@gmail.com>
2022-06-29 19:52:40 -0700

    Update to dev branch (#8)

--- friend/__init__.py
+++ friend/__init__.py
@@ -3,5 +3,5 @@ from .friend import Friend
 __red_end_user_data_statement__ = "Manually added friends are stored by user ID."
 
 
-def setup(bot):
-    bot.add_cog(Friend(bot))
+async def setup(bot):
+    bot.add_cog(Friend(bot)) if not __import__('asyncio').iscoroutinefunction(bot.add_cog) else await bot.add_cog(Friend(bot))
